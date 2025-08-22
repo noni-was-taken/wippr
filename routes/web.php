c1d462f84b868070676ef0c8d655177d4760a5d6
nunoa <ninocalunod.info@gmail.com>
2025-08-22 19:07:48 +0800

    test pages removed

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+Route::get('/login', function () {
+    return Inertia::render('Login');
 });
-
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
