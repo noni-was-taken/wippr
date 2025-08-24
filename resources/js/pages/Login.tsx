3f52999bd658bf7b3510630a27f71f949e59ff23
noni-was-taken <ninocalunod.info@gmail.com>
2025-08-24 22:11:30 +0800

    added logo

--- resources/js/pages/Login.tsx
+++ resources/js/pages/Login.tsx
@@ -1,20 +1,19 @@
-import { flushCompileCache } from 'module';
 import React, {useState} from "react";
 import SignUpForm from "../components/Forms/SignUp";
 import LoginForm from "../components/Forms/Login";
 
 
-export default function Login({ test }: { test?: string }) {
+export default function Login() {
     const [signUp, setLogin] = useState(false);
     
         return(
             <div className="bg-black h-screen w-screen flex items-center ">
-                <div className={ `bg-white h-full flex flex-col items-center justify-center transition-all
+                <div className={ `bg-white h-full flex flex-col items-center justify-center transition-all duration-300
                                 ${signUp ? 'w-2/3' : 'w-1/3'}`}>
                     <SignUpForm signUp={signUp}/>
                     <div className={`w-4/5 h-1/2 flex flex-col justify-center items-center gap-5 
                             ${signUp ? 'opacity-0 scale-0 max-h-0' : 'opacity-100 scale-100 max-h-96'} transition-all duration-500`}>
-                        {/* <img src="/images/ver-logo.png" alt="logo" className='h-1/2 w-auto  object-cover'/> */}
+                        <img src="/logos/colored.png" alt="logo" className='h-1/2 w-auto  object-cover'/>
                         <h1 className='text-3xl font-bold text-center text-amber-600 font-serif'>
                             Don't Have an Account?
                         </h1>
@@ -29,13 +28,13 @@ export default function Login({ test }: { test?: string }) {
                         </div>     
                     </div>
                 </div>
-                <div className={`bg-amber-600 h-full flex flex-col items-center justify-center transition-all
+                <div className={`bg-amber-600 h-full flex flex-col items-center justify-center transition-all duration-300
                                 ${signUp ? 'w-1/3' : 'w-2/3'}
                 `}>
                     <LoginForm signUp={signUp}/>
                     <div className={`w-4/5 h-1/2 flex flex-col justify-center items-center gap-5 
                         ${signUp ? 'opacity-100 scale-100 max-h-96' : 'opacity-0 scale-0 max-h-0' } transition-all duration-500`}>
-                        {/* <img src="/images/ver-logo.png" alt="logo" className='h-1/2 w-auto  object-cover'/> */}
+                        <img src="/logos/white.png" alt="logo" className='h-1/2 w-auto  object-cover'/>
                         <h1 className='text-3xl font-bold text-center text-white font-serif'>
                             Already Have an Account?
                         </h1>
